3f5d40c31bd07479beb1432105288c68594c525c
ayoub raji <ayoubossraji@gmail.com>
2023-03-19 16:18:58 +0000

    feat : preview selected movie + refactoring

--- AwesomeMoviesApp/force-app/main/default/lwc/movieTile/movieTile.js
+++ AwesomeMoviesApp/force-app/main/default/lwc/movieTile/movieTile.js
@@ -2,4 +2,11 @@ import { LightningElement , api} from 'lwc';
 
 export default class MovieTile extends LightningElement {
     @api movie;
+
+    handleClick() {
+        const selectedEvent = new CustomEvent('selected', {
+            detail: this.movie
+        });
+        this.dispatchEvent(selectedEvent);
+    }
 }
