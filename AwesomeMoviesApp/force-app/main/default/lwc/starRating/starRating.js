a672874ab72455c795f5546f50efeac0e8149b7f
ayoub raji <ayoubossraji@gmail.com>
2023-03-22 02:38:53 +0000

    refactore : fix style and missing data

--- AwesomeMoviesApp/force-app/main/default/lwc/starRating/starRating.js
+++ AwesomeMoviesApp/force-app/main/default/lwc/starRating/starRating.js
@@ -1,5 +1,37 @@
 import { LightningElement,api } from 'lwc';
 
 export default class StarRating extends LightningElement {
-    @api stars;
+   _rating;
+   stars = [];
+   @api
+    get rating() { return this._rating}
+    set rating(value) { 
+        this._rating = value;
+        this.generateStars();
+    }
+   
+    
+    connectedCallback() {
+        this.generateStars();
+    }
+   
+    generateStars() {
+        this.stars = [];
+        for (let i = 1; i < 6; i++) {
+           
+          const isActive = (i <= this.rating);
+          let starStyle = this.styleStar(isActive);
+          const star = {
+            id: i,
+            isActive,
+            starStyle
+          }
+          this.stars.push(star);
+        }
+    } 
+    
+    styleStar(isActive) { 
+        return isActive ? 'active-star' : 'inactive-star';
+    }
+
 }
